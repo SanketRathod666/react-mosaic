c7b4f7f52b8bbe95bf1fad7ab247419a32ce0c1e
Sanket Rathod <srathod@argusoft.com>
2024-11-29 13:04:07 +0530

    logs

--- src/MosaicDropTarget.tsx
+++ src/MosaicDropTarget.tsx
@@ -18,17 +18,35 @@ export function MosaicDropTarget({ path, position }: MosaicDropTargetProps) {
     drop: (item: MosaicDragItem | undefined, _monitor): MosaicDropData => {
       console.log(item);
       console.log(_monitor);
+      console.log(_monitor.getClientOffset());
+      
+
+      console.log(mosaicId);
+      console.log(path);
+      console.log(position);
+      console.log("sanket");
+      
       
       if (mosaicId === item?.mosaicId) {
+        console.log("line 29");
+        
         return { path, position };
       } else {
+        console.log("line 33");
         return {};
       }
     },
-    collect: (monitor) => ({
-      isOver: monitor.isOver(),
-      draggedMosaicId: (monitor.getItem() || {}).mosaicId,
-    }),
+    collect: (monitor) => {
+
+      console.log(monitor);
+      console.log(monitor.isOver());
+      console.log(monitor.getItem());      
+      
+      return {
+        isOver: monitor.isOver(),
+        draggedMosaicId: (monitor.getItem() || {}).mosaicId,
+      }
+    },
   });
   return (
     <div
